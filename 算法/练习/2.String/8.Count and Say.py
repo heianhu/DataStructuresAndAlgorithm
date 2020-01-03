5e5f8a40e0981893f9a67dbe15348c19f8d053d0
马晟 <masheng@corp.netease.com>
2020-01-03 20:13:15 +0800

    no message

--- 算法/练习/2.String/8.Count and Say.py
+++ 算法/练习/2.String/8.Count and Say.py
@@ -3,11 +3,16 @@
 """
 The count-and-say sequence is the sequence of integers with the first five terms as following:
 
-1.     1
-2.     11
-3.     21
-4.     1211
-5.     111221
+ 1.     1
+ 2.     11
+ 3.     21
+ 4.     1211
+ 5.     111221
+ 6.     312211
+ 7.     13112221
+ 8.     1113213211
+ 9.     31131211131221
+10.     13211311123113112211
 1 is read off as "one 1" or 11.
 11 is read off as "two 1s" or 21.
 21 is read off as "one 2, then one 1" or 1211.
@@ -27,9 +32,29 @@ Example 2:
 
 Input: 4
 Output: "1211"
-Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means fre
+Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
 """
 
 
 class Solution:
     def countAndSay(self, n: int) -> str:
+        result = '1'
+        for i in range(1, n):
+            count = 0
+            last_char = result[0]
+            tmp_result = ''
+            for char in result:
+                if last_char != char:
+                    tmp_result += str(count) + last_char
+                    last_char = char
+                    count = 1
+                else:
+                    count += 1
+            tmp_result += str(count) + last_char
+            result = tmp_result
+        return result
+
+
+if __name__ == '__main__':
+    test = Solution()
+    print(test.countAndSay(10))
--- 算法/练习/2.String/9.Longest Common Prefix.py
+++ 算法/练习/2.String/9.Longest Common Prefix.py
@@ -23,3 +23,25 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+        result = strs[0]
+        for current_str in strs:
+            for result_index in range(len(result), 0, -1):
+                if result[:result_index] == current_str[:result_index]:
+                    result = result[:result_index]
+                    break
+            else:
+                result = ''
+                break
+        return result
+
+
+if __name__ == '__main__':
+    test = Solution()
+    print(test.longestCommonPrefix(["flower","flow","flight"]))
+    print(test.longestCommonPrefix(["dog","racecar","car"]))
+    print(test.longestCommonPrefix([]))
+    print(test.longestCommonPrefix(['a']))
+    print(test.longestCommonPrefix(['c', 'c']))
+    print(test.longestCommonPrefix(["c","acc","ccc"]))
